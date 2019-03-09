0d469e7ae9daa0ae19ed73470d6815b4ac803069
Anastasiya_Trayanava <a.trayanava@gmail.com>
2019-03-09 19:26:42 +0300

    Clean up console project

--- FileWatch.Console/Program.cs
+++ FileWatch.Console/Program.cs
@@ -12,37 +12,7 @@ namespace FileWatch.Console
 	{
 		static void Main(string[] args)
 		{
-			FileSystemWatch fs = new FileSystemWatch(new FileSystemFactory(), y => !y.Name.Contains("5"));
-			fs.Start += Event_Start;
-			fs.Finish += Event_End;
-			fs.FilteredFileFound += Event_DelteElement;
 
-			var x = fs.CreateFileSequence(@"D:\Test");
-
-			foreach (var i in x)
-			{
-				System.Console.WriteLine(i.Name);
-			}
-		}
-
-		public static void Event_Start(object sender, EventArgs e)
-		{
-			System.Console.WriteLine("Search started");
-		}
-
-		public static void Event_End(object sender, EventArgs e)
-		{
-			System.Console.WriteLine("Search ended");
-		}
-
-		public static void Event_DelteElement(object sender, FilteredFileFoundEventArgs e)
-		{
-			if (e.CurrentUnit.Name.Contains("1"))
-			{
-				var app = sender as FileSystemWatch;
-				app.IsForDelete = true;
-				System.Console.WriteLine($"Deleted {e.CurrentUnit.Name}");
-			}
 		}
 	}
 }
