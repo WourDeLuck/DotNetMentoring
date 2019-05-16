7592fb423d156fac3553679d8d74dfb910b34029
WourDeLuck <thesakurawolf@gmail.com>
2019-05-16 05:31:40 +0300

    add base architecture for task 1

---
+++ WebsiteSaver/Enums/DomainLimitEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteSaver.Enums
+{
+	public enum DomainLimitEnum
+	{
+		NoLimit = 1,
+		InsideCurrentDomainOnly = 2,
+		InsideOriginalUrlOnly = 3
+	}
+}
