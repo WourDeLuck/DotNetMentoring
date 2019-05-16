72bbb1737421996add37f547d3f8a16439598e09
WourDeLuck <thesakurawolf@gmail.com>
2019-05-16 05:40:37 +0300

    add basic architecture for task 1

---
+++ WebsiteSaver/Helpers/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebsiteSaver.Helpers
+{
+	public static class Log
+	{
+		public static void Error(string message)
+		{
+			Console.WriteLine($"Error: {message}");
+		}
+
+		public static void Info(string message)
+		{
+			Console.WriteLine($"Info: {message}");
+		}
+	}
+}
